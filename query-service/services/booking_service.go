019eb7c5a574f095a27655f5197d25e4fd4e69b3
khalil <khalil.ait.nouisse1@gmail.com>
2026-01-31 01:39:40 +0100

    FIX : Cleaned the booking service by decomposing it into modular functions

--- query-service/services/booking_service.go
+++ query-service/services/booking_service.go
@@ -28,15 +28,40 @@ func NewBookingService(pg *pgxpool.Pool, r *redis.Client, m *mongo.Collection) *
 	}
 }
 
+// 1. The Public API for Booking
 func (s *BookingService) ProcessTicketRequest(req models.TicketRequest) error {
-
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	// 1 - Hard Transaction postgres (ACID)
+	booking, err := s.executePurchase(ctx, req)
+	if err != nil {
+		//transaction failed (sold out , DB error)
+		return err
+	}
+
+	// We do NOT update Redis or Mongo for failed bookings.
+	if booking.Status == "SOLD_OUT" {
+		log.Printf("Request %s was SOLD OUT. Skipping CQRS updates.", req.RequestID)
+		return nil // Return nil so RabbitMQ knows we handled it (don't retry)
+	}
+
+	// 2 - CQRS Softe Update (Redis , Mongo)
+	err = s.updateReadModels(ctx, req, booking)
+	if err != nil {
+		log.Printf("Soft Update Failed : %v", err)
+	}
+
+	return nil
+
+}
+
+// 2. The Private Helper for Postgres (The ACID Logic)
+func (s *BookingService) executePurchase(ctx context.Context, req models.TicketRequest) (*models.Booking, error) {
 	//start transaction
 	tx, err := s.Postgres.Begin(ctx)
 	if err != nil {
-		return fmt.Errorf("failed to begin transaction: %v", err)
+		return nil, fmt.Errorf("failed to begin transaction: %v", err)
 	}
 
 	//rollback automatically if we retrn error before commit
@@ -58,9 +83,9 @@ func (s *BookingService) ProcessTicketRequest(req models.TicketRequest) error {
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			log.Printf("Inventory not found: Match %d / %s", req.MatchID, req.Category)
-			return fmt.Errorf("inventory not found")
+			return nil, fmt.Errorf("inventory not found")
 		}
-		return fmt.Errorf("database read error: %v", err)
+		return nil, fmt.Errorf("database read error: %v", err)
 	}
 
 	//Quantity check
@@ -72,12 +97,20 @@ func (s *BookingService) ProcessTicketRequest(req models.TicketRequest) error {
 
 		_, err = tx.Exec(ctx, recordFailed, req.RequestID, req.UserID, req.MatchID, req.Category, req.Quantity, "SOLD_OUT", time.Now())
 		if err != nil {
-			return fmt.Errorf("database write error: %v", err)
+			return nil, fmt.Errorf("database write error: %v", err)
 		}
 
 		//commit the failed soldout booking
 		tx.Commit(ctx)
-		return nil
+		return &models.Booking{
+			BookingID: req.RequestID,
+			UserID:    req.UserID,
+			MatchID:   req.MatchID,
+			Category:  req.Category,
+			Quantity:  req.Quantity,
+			Status:    "SOLD_OUT",
+			CreatedAt: time.Now(),
+		}, nil
 	}
 
 	//Update Inventory (optimistic update)
@@ -87,41 +120,36 @@ func (s *BookingService) ProcessTicketRequest(req models.TicketRequest) error {
 
 	cmdTag, err := tx.Exec(ctx, queryUpdate, req.Quantity, inventory.InventoryID, inventory.Version)
 	if err != nil {
-		return fmt.Errorf("update failed: %v", err)
+		return nil, fmt.Errorf("update failed: %v", err)
 	}
 
 	//Detect Race Condition
-	//Upgrading later to " Redis Atomic Counters) "
+	//TODO : Upgrade later to " Redis Atomic Counters) "
 	if cmdTag.RowsAffected() == 0 {
 		// The version changed, so our update was ignored.(someone else bought the ticket)
 		log.Printf("RACE CONDITION DETECTED (Version Mismatch): Match %d / %s", req.MatchID, req.Category)
-		return fmt.Errorf("race condition detected") //returnin error to rabbitMQ to retry
+		return nil, fmt.Errorf("race condition detected") //returnin error to rabbitMQ to retry
 	}
 
-	//Record Booking
+	//Insert Booking
 	queryBook := `INSERT INTO bookings (booking_id, user_id, match_id, category, quantity, status, created_at) 
                   VALUES ($1, $2, $3, $4, $5, $6, $7)`
 
 	_, err = tx.Exec(ctx, queryBook, req.RequestID, req.UserID, req.MatchID, req.Category, req.Quantity, "CONFIRMED", time.Now())
 	if err != nil {
-		return fmt.Errorf("database write error: %v", err)
+		return nil, fmt.Errorf("booking insert failed: %v", err)
 	}
 
-	//Commit PostgresTransaction
+	//commit transaction
 	err = tx.Commit(ctx)
 	if err != nil {
-		return fmt.Errorf("commit failed: %v", err)
+		return nil, fmt.Errorf("commit failed: %v", err)
 	}
 
 	log.Printf("Postgres Transaction Committed for Request %s", req.RequestID)
-	log.Printf("SUCCESS: Booked %d tickets for Match %d - Category %s", req.Quantity, req.MatchID, req.Category)
+	log.Printf("Booked %d tickets for Match %d - Category %s", req.Quantity, req.MatchID, req.Category)
 
-	//Update Redis Cache
-	redisKey := fmt.Sprintf("inventory:%d:%s", req.MatchID, req.Category)
-	s.Redis.DecrBy(ctx, redisKey, int64(req.Quantity))
-
-	//Update MongoDB
-	bookingDoc := models.Booking{
+	return &models.Booking{
 		BookingID: req.RequestID,
 		UserID:    req.UserID,
 		MatchID:   req.MatchID,
@@ -129,9 +157,19 @@ func (s *BookingService) ProcessTicketRequest(req models.TicketRequest) error {
 		Quantity:  req.Quantity,
 		Status:    "CONFIRMED",
 		CreatedAt: time.Now(),
+	}, nil
+}
+
+func (s *BookingService) updateReadModels(ctx context.Context, req models.TicketRequest, booking *models.Booking) error {
+
+	//Update Redis Cache
+	redisKey := fmt.Sprintf("match:%d:category:%s", req.MatchID, req.Category)
+	err := s.Redis.DecrBy(ctx, redisKey, int64(req.Quantity)).Err()
+	if err != nil {
+		log.Printf("Redis Update Failed: %v", err)
 	}
 
-	_, err = s.Mongo.InsertOne(ctx, bookingDoc)
+	_, err = s.Mongo.InsertOne(ctx, booking)
 	if err != nil {
 		log.Printf("Mongo Update Failed (User won't see ticket yet): %v", err)
 	}
